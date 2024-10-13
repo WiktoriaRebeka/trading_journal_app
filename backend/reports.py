41283ce3ec702f6c849b6e66bc6ddbae3bb33c7d
Wiktoria <wiktoriarebeka00@gmail.com>
2024-10-13 11:42:10 +0200

    Changing language

--- backend/reports.py
+++ backend/reports.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import JournalEntry
 from .utils import calculate_report_data, get_report_for_period
+import plotly.graph_objects as go
 
 @login_required
 def reports_view(request):
@@ -14,12 +15,19 @@ def reports_view(request):
     weekly_report = get_report_for_period(journal_entries, days=7)
     daily_report = get_report_for_period(journal_entries, days=1)
 
+      # Tworzenie wykresu kołowego (pie chart) dla ogólnego raportu
+    pie_fig = go.Figure(data=[go.Pie(labels=['Win', 'Lose'], 
+                                     values=[total_report['yes_count'], total_report['no_count']])])
+    pie_chart_html = pie_fig.to_html(full_html=False)
+
+
     # Przekazanie raportów do szablonu
     return render(request, 'app_main/reports.html', {
         'total_report': total_report,
         'monthly_report': monthly_report,
         'weekly_report': weekly_report,
         'daily_report': daily_report,
+        'pie_chart_html': pie_chart_html,  # Przekazanie wykresu do szablonu
     })
 
 @login_required
